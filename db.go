02a72e72f77c1d71fddbc0324861422bc43d6be2
zanjs <root@zanjs.com>
2017-05-14 18:55:28 +0800

    Those who turn back never reach the summit. :smirk:

--- db.go
+++ db.go
@@ -4,31 +4,48 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/configor"
 	"github.com/jinzhu/gorm"
 )
 
 var Idb *gorm.DB
 
+var Config = struct {
+	APPName string `default:"app name"`
+
+	DB struct {
+		Name     string
+		User     string `default:"root"`
+		Password string `required:"true" env:"DBPassword"`
+		Port     string `default:"3306"`
+		Host     string `default:"127.0.0.1"`
+		Option   string
+	}
+}{}
+
 func init() {
 	var err error
 
-	Idb, err = gorm.Open("mysql", "root:root@tcp(127.0.0.1:3306)/gotest?charset=utf8&parseTime=True")
-	//Idb, err := gorm.Open("mysql", "root:root@/gotest?charset=utf8&parseTime=True&loc=Local")
+	configor.Load(&Config, "config.yml")
+
+	fmt.Printf("config : %#v", Config)
+
+	uDB := Config.DB
+
+	uri := uDB.User + ":" + uDB.Password + "@tcp(" + uDB.Host + ":" + uDB.Port + ")/" + uDB.Name + "?" + uDB.Option
+
+	Idb, err = gorm.Open("mysql", uri)
 	if err != nil {
 		panic("failed to connect database")
 	}
-	//Idb2,_ = gorm.Open("mysql", "root:root@/gotest?charset=utf8&parseTime=True&loc=Local")
 	defer Idb.Close()
 	// Migrate the schema
 	Idb.AutoMigrate(&Product{})
 	// Create
 	Idb.Create(&Product{Code: "L1212", Price: 1000})
-
 	// Read
 	var product Product
 	//Idb.First(&product, 1) // find product with id 1
-
 	Idb.Find(&product)
-
 	fmt.Println(product)
 }
